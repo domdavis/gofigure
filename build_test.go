8819ea2b4f666296875e2c198c9a63ac4092473d
Dom Davis <dom@domdavis.com>
2023-04-04 10:34:47 +0100

    Fix #3: Add build variables

---
+++ build_test.go
@@ -0,0 +1,14 @@
+package gofigure_test
+
+import (
+	"fmt"
+
+	"bitbucket.org/idomdavis/gofigure"
+)
+
+func ExampleBuild() {
+	fmt.Println(gofigure.Build())
+
+	// Output:
+	// dev [<unset>] (built: <unset>)
+}
