aceb69acfc7ff8498b3a7a00cd7fa059e92184f3
Dom Davis <dom@domdavis.com>
2023-04-04 10:14:16 +0100

    Issue #2: Update package documentation

--- doc.go
+++ doc.go
@@ -1,2 +1,3 @@
-// Package gofigure requires a description.
+// Package gofigure allows for configuration of an application using command
+// line flags, environment variables and configuration files.
 package gofigure
